ba3da8b8c1f3032b55dc1f37d889ec25fcc5c1ab
amnah1438 <__@domain.com>
2025-11-11 01:18:09 +0300

    تعديل الفيو في البرودكت

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    """
+    عرض قائمة المنتجات في المتجر
+    """
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'products/list.html', context)
