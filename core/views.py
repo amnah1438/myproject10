ea4ddfe080a9ec2c6228349320d926faf324b23a
amnah1438 <__@domain.com>
2025-11-10 18:19:27 +0300

    تعديل فيو

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1>مرحبًا بكِ في الصفحة الرئيسية للمتجر الإلكتروني 🛍️</h1>")
