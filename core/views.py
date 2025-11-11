3741fb34ffe845141c30b86169669634dc6ea57e
amnah1438 <__@domain.com>
2025-11-11 12:59:45 +0300

    تعديل الفيو في الكور

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from products.models import Product  # ✅ استدعاء الموديل من تطبيق المنتجات
 
 def home(request):
-    return render(request, 'home.html')
+    """
+    الصفحة الرئيسية تعرض المنتجات من قاعدة البيانات
+    """
+    products = Product.objects.all()  # 🛒 جلب المنتجات من قاعدة البيانات
+    context = {'products': products}
+    return render(request, 'products-templates/list.html', context)
